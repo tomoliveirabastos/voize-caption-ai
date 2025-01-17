b3441933666619d9bc4ddcbf981b12bbc52684bb
Tom Oliveira <washington-kd@hotmail.com>
2025-01-16 22:17:24 -0300

    M translate_object.py

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
-from argostranslate import translate
+from argostranslate import translate, package
 
 def translate_to_portuguese(text: str) -> str:
     from_code = 'en'
     to_code = 'pt'    
 
+    package.update_package_index()
+    available_packages = package.get_available_packages()
+    package_to_install = next(
+        filter(
+            lambda x: x.from_code == from_code and x.to_code == to_code, available_packages
+        )
+    )
+    package.install_from_path(package_to_install.download())
     print(text, from_code, to_code)
     return translate.translate(text, from_code, to_code)
 
